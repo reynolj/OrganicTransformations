6ba055f956200449be5057b505d26c606335f596
sanchejj <joeljsanchez@csus.edu>
2020-02-17 15:39:44 -0800

    Started the account settings section. Edit Profile can be used as a skeleton for the other sections.

---
+++ pn/account_settings.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>OT | Account Settings</title>
+
+	<!-- Tell the browser to be responsive to screen width -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- Font Awesome -->
+	<link rel="stylesheet" href="AdminLTE/plugins/fontawesome-free/css/all.min.css">
+	<!-- Ionicons -->
+	<link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+	<!-- icheck bootstrap -->
+	<link rel="stylesheet" href="AdminLTE/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+	<!-- Theme style -->
+	<link rel="stylesheet" href="AdminLTE/dist/css/adminlte.min.css">
+	<!-- Google Font: Source Sans Pro -->
+	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+</head>
+<body class="sidebar-light-white" style="height: auto;">
+	<!-- Navbar -->
+	<nav class="navbar navbar-expand navbar-white navbar-light">
+		<!-- Logo -->
+		<a class="navbar-brand" href="#">
+			<img src="https://via.placeholder.com/45x45.png?text=OT">
+		</a>
+		<!-- Logout Button -->
+		<ul class="navbar-nav ml-auto">
+			<li class="nav-item d-none d-sm-inline-block">
+				<a class="nav-link" href="../front/index.html">Logout</a>
+			</li>
+		</ul>
+	</nav>
+	<!-- The rest below the header bar -->
+	<div class="wrapper">
+		<!-- Sidebar -->
+		<aside class="main-sidebar">
+
+		</aside>
+
+		<!-- Content for Edit Profile. -->
+		<!-- To start your own section, copy from here -->
+		<div class="content-wrapper">
+			<!-- Head -->
+			<section class="content-header">
+				<div class="container-fluid">
+					<div class="row">
+						<h1>Edit Profile</h1>
+					</div>
+				</div>
+			</section>
+			<!-- Main Body -->
+			<div class="card">
+				<div class="card-body">
+					<!-- to here, roughly. Change what is needed -->
+					<!-- Input Row -->
+					<div class="form-group row">
+						<div class="input-group">
+							<!-- Label -->
+							<label class="col-sm-4 col-form-label" for="first_name last_name">First & Last Name</label>
+							<!-- Input box -->
+							<div class="col-sm-4">
+								<input class="form-control" id="first_name" placeholder="First Name"/>
+							</div>
+							<div class="col-sm-4">
+								<input class="form-control" id="last_name" placeholder="Last Name"/>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+	</div>
+</body>
+</html>
