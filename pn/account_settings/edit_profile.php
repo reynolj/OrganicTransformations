0c17a11bb369381081c64f36f8bb5a27e8871a0f
sanchejj <joeljsanchez@csus.edu>
2020-02-19 10:26:16 -0800

    Finished the basic layout of the edit profile tab

--- pn/account_settings/edit_profile.php
+++ pn/account_settings/edit_profile.php
@@ -21,7 +21,7 @@
 </head>
 <body class="container-fluid" style="height: auto;">
 	<!-- Navbar -->
-	<nav class="navbar navbar-expand navbar-white navbar-light">
+	<nav class="navbar navbar-expand navbar-white navbar-light elevation-4">
 		<!-- Logo -->
 		<a class="navbar-brand" href="#">
 			<img src="https://via.placeholder.com/45x45.png?text=OT">
@@ -83,17 +83,75 @@
 			<div class="card ml-3 mr-3">
 				<div class="card-body">
 					<!-- Input Row -->
-					<div class="form-group row">
-						<div class="input-group">
-							<!-- Label -->
-							<label class="col-sm-4 col-form-label" for="first_name last_name">First & Last Name</label>
-							<!-- Input box -->
-							<div class="col-sm-4">
-								<input class="form-control" id="first_name" placeholder="First Name"/>
+					<div class="form-group">
+						<div class="row">
+							<div class="input-group">
+								<!-- First/Last Name Label -->
+								<label class="col-sm-4 col-form-label" for="first_name last_name">First & Last Name</label>
+								<!-- First Name Input box -->
+								<div class="col-sm-4">
+									<input class="form-control" id="first_name" placeholder="First Name"/>
+								</div>
+								<!-- Last Name Input box -->
+								<div class="col-sm-4">
+									<input class="form-control" id="last_name" placeholder="Last Name"/>
+								</div>
 							</div>
+						</div>
+
+						<div class="row">
+							<!-- Email Label -->
+							<label class="col-sm-8 col-form-label" for="email_address">Email Address</label>
+							<!-- Email Input box -->
 							<div class="col-sm-4">
-								<input class="form-control" id="last_name" placeholder="Last Name"/>
+								<input class="form-control" id="email_address" placeholder="johndoe@gbox.com"/>
 							</div>
+						</div>
+
+						<div class="row">
+							<!-- Phone Number Label -->
+							<label class="col-sm-10 col-form-label" for="phone_number">Phone Number</label>
+							<!-- Phone Number Input box -->
+							<div class="col-sm-2">
+								<input class="form-control" id="phone_number" placeholder="+1 (123) 456 - 7890"/>
+							</div>
+						</div>
+
+						<div class="row">
+							<!-- Gender Label-->
+							<label class="col-sm-10 col-form-label" for="gender">Gender</label>
+							<!-- Gender Input Box-->
+							<div class="col-sm-2">
+									<select class="form-control" id="gender">
+										<option>Male</option>
+										<option>Female</option>
+										<option>Other</option>
+									</select>
+							</div>
+						</div>
+
+						<div class="row">
+							<!-- Date of Birth Label-->
+							<label class="col-form-label col-sm-8" for="month day year">Date of Birth</label>
+							<!-- Date of Birth Input Box-->
+							<select class="form-control col-sm-2" id="month">
+								<option>January</option>
+								<option>February</option>
+								<option>March</option>
+								<option>April</option>
+								<option>May</option>
+								<option>June</option>
+								<option>July</option>
+								<option>August</option>
+								<option>September</option>
+								<option>November</option>
+								<option>December</option>
+							</select>
+							<input class="form-control col-sm-1" id="day" placeholder="Day">
+							<input class="form-control col-sm-1" id="year" placeholder="Year">
+						</div>
+
+							<!-- Save Changes -->
 						</div>
 					</div>
 				</div>
