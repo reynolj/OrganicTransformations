e9fc5e12f87d4d7cc972f93d837261244feaf33d
Jason Chernioglo <jasoncher@Jasons-MacBook-Pro.local>
2020-02-24 21:00:11 -0800

    Fixed variables.php issue

--- pn/api/auth/login.php
+++ pn/api/auth/login.php
@@ -1,7 +1,7 @@
 <?php
 //If already logged in, take them to the home page
 session_start();
-if(isset($_SESSION['user_id']) && !empty($_SESSION['user_id']) ){
+if(isset($_SESSION['user_id']) || !empty($_SESSION['user_id']) ){
    header("Location: index.php");
    exit();
 }
--- pn/api/auth/register.php
+++ pn/api/auth/register.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once('../../../variables.php');
+require_once('../../variables.php');
 
 
 if (   !isset($_POST['username'])
