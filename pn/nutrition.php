8b4bb77f25786328bb7ec99d14e6cd98f62c0a8a
cbotonis <cbotonis@csus.edu>
2020-03-07 10:34:22 -0800

    started mealCalc in backend api folder to calculate meal plans, made some html changes to nutrition.php mostly layout and changing some of the input fields. Started writing js script to push the values to database.

--- pn/nutrition.php
+++ pn/nutrition.php
@@ -5,9 +5,59 @@ $highlight = "nutrition"; //Select which tab in the navigation to highlight
 require("structure/top.php"); //Include the sidebar HTML
 ?>
 
-  <scrip type="text/javascript">
-    <!-- Put Javascript Here -->
-  </scrip>
+  <script type="text/javascript">
+    function calculate(){
+    //Check if all the required fields are filled out
+          if($('#blood_type :selected').text() == ""
+            || $('#body_type :selected').text() == ""
+            || $('#current_weight').val() == ""
+            || $('#target_fat').val() == ""
+            || $('#sex :selected').text() == ""
+            || $('#desired_outcome :selected').text() == ""
+            || $('#current_fat').val() == ""
+            ){
+            $('#statusMsg').html("Make sure to fill out every field.");
+            return;
+          }
+          //Check the username length
+          if( $('#current_weight').val() < 1 || $('#current_weight').val() > 1000){
+            $('#statusMsg').html("PLease enter a valid weight");
+            return;
+          }
+
+          if( $('#target_fat').val() < 1 || $('#current_fat').val() > 100){
+          $('#statusMsg').html("Please enter a valid target fat level");
+          return;
+          }
+
+          //Disable the login button
+          $('#calcBtn').prop('disabled', true);
+
+          //Send the form data
+          $.ajax({
+            type: "POST",
+            dataType: 'text',
+            url: 'api/auth/register.php',
+            data: {
+              blood_type: $('#blood_type :selected').text(),
+              body_type: $('#body_type :selected').text(),
+              current_weight: $('#current_weight').val(),
+              target_fat: $('#target_fat').val(),
+              sex: $('#sex :selected'),
+              desired_outcome: $('#desired_outcome :selected').text() == ""
+              current_fat: $('#current_fat').val()
+            },
+            success: function(data, status){
+              if(data == "success"){
+                window.location.replace("login.php?register_success");
+              }else{
+                $('#statusMsg').html(data);
+                $('#loginBtn').prop('disabled', false);
+              }
+            }
+          });
+        }
+  </script>
 
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
@@ -32,83 +82,87 @@ require("structure/top.php"); //Include the sidebar HTML
     <div class="content">
       <div class="container-fluid">
 
-            <div class="card card-primary card-outline">
-              <div class="card-header">
-                <h5 class="m-0">Nutrition Planner</h5>
-              </div>
-              <div class="card-body">
+
+             <div class="card-body">
                 <!-- Place page content here -->
                 <h3><p>To determine a nutrition plan that will best work for you we need to gather some information about you</p></h3>
+              <div class="row">
+                <div class="col-sm-4">
+                    <div class="form-group">
+                          <label>Select your blood type.</label>
+                           <select id="blood_type" class="form-control">
+                               <option>I don&apos;t know my blood type</option>
+                               <option>A-</option>
+                               <option>A+</option>
+                               <option>B-</option>
+                               <option>B+</option>
+                               <option>AB-</option>
+                               <option>AB+</option>
+                               <option>O-</option>
+                               <option>O+</option>
+                           </select>
+                    </div>
+                </div>
 
-                <div class="row">
-                    <div class="col-sm-4">
-                        <div class="form-group">
-                            <label>My age(in years):</label>
-                            <input type="text" class="form-control" placeholder="Enter your age here" required>
-                            </div>
-                           </div>
-                    <div class="col-sm-4">
-                        <div class="form-group">
-                           <label>My weigth(lbs):</label>
-                              <input type="text" class="form-control" placeholder="Enter your weight here">
-                             </div>
-                          </div>
-                    <div class="col-sm-4">
-                        <div class="form-group">
-                            <label>Target weigth (lbs):</label>
-                            <input type="text" class="form-control" placeholder="Enter your target weight here">
-                        </div>
+                <div class="col-sm-4">
+                  <div class="form-group">
+                    <label>My weigth(lbs):</label>
+                       <input id = "current_weight" type="number" class="form-control" placeholder="Enter your weight here">
                     </div>
                   </div>
-                <div class="row">
-                    <div class="col-sm-4">
-                       <div class="form-group">
+                <div class="col-sm-4">
+                  <div class="form-group">
+                       <label>Target body fat:</label>
+                           <input id="target_fat" type="number" class="form-control" placeholder="Enter your target body fat percentage here">
+                  </div>
+                </div>
+              </div><!-- /row -->
+              <div class="row">
+                <div class="col-sm-4">
+                  <div class="form-group">
                          <label>Sex</label>
-                          <select class="form-control">
-                          <option>Male</option>
-                          <option>Female</option>
-                          </select>
-                         </div>
-                    </div>
-                    <div class="col-sm-4">
-                        <div class="form-group">
+                           <select id="sex" class="form-control">
+                           <option>Male</option>
+                           <option>Female</option>
+                         </select>
+                  </div>
+                </div>
+                <div class="col-sm-4">
+                   <div class="form-group">
                         <label>Desired outcome</label>
-                        <select class="form-control">
-                        <option>Burn fat/lose weight</option>
-                        <option>Build muscle/bulk up</option>
-                        <option>Get shredded (below 8% body fat)</option>
-                        <option>Increase performance/athleticism</option>
+                        <select id="desired_outcome" class="form-control">
+                         <option>Burn fat/lose weight</option>
+                         <option>Build muscle/bulk up</option>
+                         <option>Get shredded (below 8% body fat)</option>
+                         <option>Increase performance/athleticism</option>
                         </select>
-                        </div>
-                    </div>
+                   </div>
+                </div>
                 <div class="col-sm-4">
                     <div class="form-group">
                         <label>I want to accomplish my goal in:</label>
-                        <select class="form-control">
+                        <select id="timeline" class="form-control">
                         <option>No time line</option>
                         <option>Less than 2 months</option>
                         <option>2-4 months</option>
                         <option>4-6 months</option>
                         </select>
-                      </div>
+                    </div>
                 </div>
 
-                </div>
+              </div><!-- /.row -->
                 <h3><p>Please select the body type that most resembles yours based on the graphic and description</p></h3>
 
                 <div class="row">
                    <div class="col-md-4">
                       <div class="card">
                          <div class="card-header">
-                              <h3 class="card-title">
-                                 Ectomorph
-                               </h3>
-                         </div>
-                              <!-- /.card-header -->
+                              <h3 class="card-title">Ectomorph</h3>
+                         </div><!-- /.card-header -->
                               <div class="card-body" style="text-align:center;">
                                 <div>
-                                <img style='height: 60%; width: 60%; object-fit: contain' src="images/ectos.jpg">
-                                 </div>
+                                   <img style='height: 60%; width: 60%; object-fit: contain' src="images/ectos.jpg">
+                                </div>
                                 <ul style="display: inline-block; text-align: left;">
                                   <li>Skinny</li>
                                   <li>Small joints/bones</li>
@@ -190,7 +244,7 @@ require("structure/top.php"); //Include the sidebar HTML
                          <div class="col-sm-4">
                                             <div class="form-group">
                                                 <label>My body type is:</label>
-                                                <select class="form-control">
+                                                <select id="body_type"class="form-control">
                                                 <option> </option>
                                                 <option>Ectomorph</option>
                                                 <option>Mesomorph</option>
@@ -205,49 +259,26 @@ require("structure/top.php"); //Include the sidebar HTML
                              </h3>
                             <img style='height: 50%; width: 50%; object-fit: contain' src="images/fatM.jpg">
                             <img style='height: 50%; width: 50%; object-fit: contain' src="images/fatF.jpg">
-                           </div
-                            <div class="col-sm-4">
-                              <div class="form-group">
-                                <label>My body fat percentage:</label>
-                                <input type="text" class="form-control" placeholder="Enter your body fat % here" required>
-                               </div>
+                          </div>
+                          <label>My body fat percentage:</label>
+                         <div class="row">
+                           <div class="col-sm-4">
+                             <div class="form-group">
+
+                                <input id="current_fat" type="number" class="form-control" placeholder="Enter your body fat % here" required>
                              </div>
+                           </div>
 
-                             <div class="col-sm-4">
-                                 <div class="form-group">
-                                 <label>Finally we need your blood type to help determine which
-                                  foods to eat and which to avoid.</label>
-                                  <select class="form-control">
-                                                                            <option>I don&apos;t know my blood type</option>
-                                                                            <option>A-</option>
-                                                                            <option>A+</option>
-                                                                            <option>B-</option>
-                                                                            <option>B+</option>
-                                                                            <option>AB-</option>
-                                                                            <option>AB+</option>
-                                                                            <option>O-</option>
-                                                                            <option>O+</option>
-                                                                            </select>
-                                                                            </div>
-                                                                        </div>
 
-                              <div class="col-sm-4">
+
+                            <div class="col-sm-4">
                                 <td>
-                                 <button type="button" class="btn btn-block btn-primary">Calculate my nutrition plan</button>
+                                 <button id="calcBtn" onclick="calculate()" type="button" class="btn btn-block btn-primary">Calculate my nutrition plan</button>
                                 </td>
-                              </div>
+                            </div>
+                         </div>
 
-
-
-
-
-                </div>
-              </div>
-            </div>
-
-
-
-
+             </div>
       </div>
       <!-- /.container-fluid -->
     </div>
