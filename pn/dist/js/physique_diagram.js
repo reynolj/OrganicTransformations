f674288a0e8158d27c4957c280908a47a6ed9efb
AvWoN <61403188+AvWoN@users.noreply.github.com>
2020-03-10 07:01:16 -0700

    Click on Muscle now displays Modal popup, the container of the exercise guides (still needs implementation of querying and displaying the guides )

--- pn/dist/js/physique_diagram.js
+++ pn/dist/js/physique_diagram.js
@@ -9,15 +9,21 @@ $(document).ready(function () {
     $('.muscle').hover(function () {
         $('#info-box').css('display', 'block');
 
-        //Display the correct form of the name (w/o '_' for space)
-        let displayName =  $(this).attr('data-name') == undefined ? $(this).attr('id') : $(this).attr('data-name');
+        //Display the correct form of the name
+        let displayName =  getDisplayName(this);
 
         $('#info-box').html(displayName);
     });
 
     //Mouse click opens popup with guides associated with the
     $('.muscle').click(function(){
-        
+        console.log('Clicked')
+        $('#myModalLabel').html('Exercises for the <span style="font-weight: bold;">' + getDisplayName(this) + '</span>')
+        $('#myModal').modal({
+            backdrop: true,
+            keyboard: true,
+            show: true
+        })
     })
 
     //Mouse moved off muscle, hide tooltip
@@ -30,4 +36,10 @@ $(document).ready(function () {
         $('#info-box').css('left', e.pageX - ($('#info-box').width()) / 2);
     }).mouseover();
 });
+
+//Returns the correct display name (w/o '_' for space)
+function getDisplayName(e) {
+    let ret = $(e).attr('data-name') === undefined ? $(e).attr('id') : $(e).attr('data-name');
+    return ret;
+}
 
