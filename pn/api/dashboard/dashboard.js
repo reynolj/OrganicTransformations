e879bb39368ebae5d4c2b68eee9ed6e524bf3a4e
sanchejj <joeljsanchez@csus.edu>
2020-03-19 14:24:22 -0700

    246, 248: Fixed goal bug and favoriting guide issue. Guide.js created for use in any javascript, and moved all guide related files into their own folder.

---
+++ pn/api/dashboard/dashboard.js
@@ -0,0 +1,246 @@
+import Guide from '../guides/Guide.js';
+
+let favorites = Array();
+let goal_len = 0;
+
+$( window ).on( "load", function() {
+    get_favorites();
+    get_body();
+    get_goals();
+    get_guides(4, $('#nutrition_favorites'), 'nutrition', 1);
+    get_guides(4, $('#exercise_favorites'), 'exercise', 1);
+    get_guides(4, $('#highlighted_guides'), 'nutrition', -1);
+});
+
+function get_favorites() {
+    $.ajax({
+        type: 'POST',
+        url: '/pn/api/dashboard/get_favorites.php',
+        success: function(data) {
+            let json = JSON.parse(data);
+            for(let key in json) {
+                if(json.hasOwnProperty(key)) {
+                    favorites.push(json[key]['guide_id']);
+                }
+            }
+        },
+        error: function() {
+            console.log('get_favorites ERROR');
+        }
+    });
+}
+
+function get_body() {
+    $.ajax({
+        type: 'POST',
+        url: '/pn/api/nutri/get_nutrition.php',
+        success: function(data) {
+            let json = JSON.parse(data);
+            for(let key in json) {
+                if(json.hasOwnProperty(key)) {
+                    $('#blood_type').val(json[key]['blood_type']);
+                    $('#body_type').val(json[key]['body_type']);
+                    $('#weight').val(json[key]['current_weight']);
+                    $('#activity_level').val(json[key]['activity_lvl']);
+                    break;
+                }
+            }
+        },
+        error: function() {
+            console.log("Get_Body ERROR");
+        }
+    });
+}
+
+function save_body() {
+    $('#add_goal_btn').prop('disabled', true);
+    const blood_type = $('#blood_type').val();
+    const body_type = $('#body_type').val();
+    const weight = $('#weight').val();
+    const activity_level = $('#activity_level').val();
+    $.ajax({
+        type: 'POST',
+        url: '/pn/api/nutri/save_nutrition.php',
+        data: {
+            blood_type: blood_type,
+            body_type: body_type,
+            weight: weight,
+            activity_level: activity_level
+        },
+        success: function() {
+            $('#add_goal_btn').prop('disabled', false);
+        },
+        error: function() {
+            $('#add_goal_btn').prop('disabled', false);
+            console.log("Get_Body ERROR");
+        }
+    });
+}
+
+function get_goals() {
+    $.ajax({
+        type:'POST',
+        url: '/pn/api/dashboard/get_goals.php',
+        success: function(data) {
+            let goal_str = Array();
+            const json = JSON.parse(data);
+            for(let key in json) {
+                if (json.hasOwnProperty(key))
+                    goal_str.push(
+                        '<li>' +
+                            json[key]['goal'] +
+                            '<button type="button" ' +
+                                'class="close text-right" ' +
+                                'id="' + json[key]['goal'] + '" onclick="delete_goal(this)">' +
+                                '×' +
+                            '</button>' +
+                        '</li>');
+            }
+            goal_str.push(
+                '<li class="text-right" id="last_goal_line">' +
+                    '<button class="btn btn-primary" id="add_goal_btn" onclick="add_goal()" ' +
+                    'style="background-color:green;border-color:green;height:100%">' +
+                        'Add Goal' +
+                    '</button>' +
+                '</li>'
+            );
+            $('#goals').html(goal_str);
+        },
+        error: function() {
+            console.log("Get_Goals ERROR");
+        }
+    });
+}
+
+function add_goal() {
+    $('#last_goal_line').before(
+        '<li id="add_goal_line">' +
+            '<div class="row">' +
+            '<input id="goal_input" class="form-control col-11" type="text" placeholder="New Goal">' +
+                '<button class="btn btn-primary col-1" id="submit_goal_btn" onclick="submit_goal()" ' +
+                'style="background-color: blue; border-color:blue; height:100%">' +
+                    '+' +
+                '</button>' +
+            '</div>' +
+        '</li>'
+    );
+    $('#add_goal_btn').prop('disabled', true);
+}
+
+function submit_goal() {
+    const goal = $('#goal_input').val();
+    if(goal === "") {
+        return;
+    }
+    $.ajax({
+        type: 'POST',
+        url: '/pn/api/dashboard/submit_goal.php',
+        data: {
+            goal: goal
+        },
+        success: function(data) {
+            const parent = document.getElementById('goals');
+            parent.removeChild(document.getElementById('add_goal_line'));
+            $('#last_goal_line').before(
+                '<li>' + data +
+                    '<button type="button" ' +
+                    'class="close text-right" ' +
+                    'id="' + data + '" onclick="delete_goal(this)">' +
+                        '×' +
+                    '</button>' +
+                '</li>'
+            );
+            ++goal_len;
+            $('#add_goal_btn').prop('disabled', false);
+        },
+        error: function() {
+            console.log("Submit_Goal ERROR");
+        }
+    });
+}
+
+function delete_goal(button) {
+    let goal = button.id;
+    let list = document.querySelectorAll('#goals li');
+    $.ajax({
+        type: 'POST',
+        url: '/pn/api/dashboard/remove_goal.php',
+        data: {
+            goal: goal
+        },
+        success: function() {
+            for(let key in list) {
+                if(list.hasOwnProperty(key)) {
+                    if(list[key].childNodes[0].textContent === goal) {
+                        list[key].remove();
+                        break;
+                    }
+                }
+            }
+            --goal_len;
+        },
+        error: function() {
+            console.log("ERROR");
+        }
+    });
+}
+
+function get_guides(number, container, tag, favorite) {
+    $.ajax({
+        type:'POST',
+        url: '/pn/api/guides/get_guides.php',
+        data: {
+            number: number,
+            tag: tag,
+            favorite: favorite
+        },
+        success: function(data) {
+            container.html(make_cards(JSON.parse(data)));
+        },
+        error: function() {
+            console.log("get_guides ERROR");
+        }
+    });
+}
+
+function make_cards(data) {
+    let str_hold = Array();
+    let guide;
+    for(let key in data) {
+        if(data.hasOwnProperty(key)) {
+            guide = new Guide(data[key]);
+            str_hold.push(guide.card(favorites.includes(data[key]['guide_id'])));
+        }
+    }
+    return str_hold;
+}
+
+function favorite(wrapper) {
+    let classes = wrapper.classList;
+    const guide_id = wrapper.id.slice('guide'.length);
+    const favorited = classes.contains('favorite') ? 1 : 0;
+    $.ajax({
+        type:'POST',
+        url: '/pn/api/dashboard/favorite_guide.php',
+        data: {
+            guide_id: guide_id,
+            favorited: favorited
+        },
+        success: function(data) {
+            if(favorited) {
+                classes.remove('favorite');
+                favorites.splice(favorites.indexOf(guide_id), 1);
+            }
+            else {
+                classes.add('favorite');
+                favorites.push(guide_id);
+            }
+            get_guides(4, $('#nutrition_favorites'), 'nutrition', 1);
+            get_guides(4, $('#exercise_favorites'), 'exercise', 1);
+            get_guides(4, $('#highlighted_guides'), 'nutrition', -1);
+        },
+        error: function() {
+            console.log("ERROR");
+        }
+    });
+}
--- pn/api/dashboard/get_favorites.php
+++ pn/api/dashboard/get_favorites.php
@@ -1,25 +1,26 @@
 <?php
+
 require("../auth/login_check.php"); //Make sure the users is logged in
 require_once('../../variables.php');
 
 try {
     $user_id = intval($_SESSION['user_id']);
-    $goal = $_POST['goal'];
 
     $con = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
 
     //This prepared statement takes the arguments AS IS.
     //YOU MUST MAKE THEM THE RELEVANT DATA TYPES BEFORE CALLING.
     $con->setAttribute(PDO::ATTR_EMULATE_PREPARES, FALSE);
+
     $stmt = $con->prepare("
-        DELETE FROM goals
-        WHERE 
-            user_id = ? AND 
-            goal = ? 
-        LIMIT 1;
+        SELECT guide_id
+        FROM favorites
+        WHERE user_id = ?
     ");
-    $stmt->execute([$user_id, $goal]);
-}
-catch(PDOException $e) {
+    $stmt->execute([$user_id]);
+    $results = $stmt->fetchAll();
+
+    die(json_encode($results));
+} catch (PDOException $e) {
     die("Request failed");
 }
