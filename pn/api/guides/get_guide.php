cdab01d25170918c1ce838f92fe516f162babd72
jasoncher <jasoncher@gmail.com>
2020-04-03 18:23:14 -0700

    #272

--- pn/api/guides/get_guide.php
+++ pn/api/guides/get_guide.php
@@ -6,7 +6,7 @@ require_once('../../../variables.php');
 try {
     $con = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
     $con->setAttribute(PDO::ATTR_EMULATE_PREPARES, FALSE);
-    $stmt = $con->prepare("SELECT * FROM guides WHERE guide_id = ?");
+    $stmt = $con->prepare("SELECT guide_id, thumbnail, date_created, date_last_modified, guide_name, subscription_level, content FROM guides WHERE guide_id = ?");
     $stmt->execute([$_POST['guide_id']]);
 
     $data = $stmt->fetch();
@@ -20,7 +20,6 @@ try {
         $status->guide_data = $data;
         die(json_encode($status));
     }
-
 
 } catch (PDOException $e) {
     $status->result = "ERROR";
