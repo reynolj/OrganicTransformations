dee97c973c62c280fcf84ec370386bc02488af98
jasoncher <jasoncher@gmail.com>
2020-04-03 18:55:03 -0700

    #276 (Get tags backend API)

---
+++ pn/api/admin/guides/get_tags.php
@@ -0,0 +1,21 @@
+<?php
+require("../../auth/login_check.php"); //Make sure the users is logged in
+require_once('../../../variables.php');
+
+try {
+    $con = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
+    $con->setAttribute(PDO::ATTR_EMULATE_PREPARES, FALSE);
+    $stmt = $con->prepare("SELECT * FROM tags WHERE guide_id = ?");
+    $stmt->execute([$_POST['guide_id']]);
+
+    $data = $stmt->fetch();
+
+    $status->result = "SUCCESS";
+    $status->data = $data;
+    die(json_encode($status));
+
+}catch(PDOException $e) {
+    $status->result = "ERROR";
+    $status->message = "Something went wrong. Please try again later.";
+    die(json_encode($status));
+}
