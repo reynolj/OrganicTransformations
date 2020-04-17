d8701747cf550d3278936bdd9c175b658b12bb87
Vina <malvinamarkoglo@csus.edu>
2020-04-16 23:54:03 -0700

    removed AdminLTE App and Demo plugins that were breaking the nav bar collapse on counseling and members pages

--- pn/members.php
+++ pn/members.php
@@ -8,7 +8,6 @@ require("structure/top.php"); //Include the sidebar HTML
 <html style="height: auto;"><head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>AdminLTE 3 | DataTables</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -125,10 +124,6 @@ require("structure/top.php"); //Include the sidebar HTML
 <!-- DataTables -->
 <script src="AdminLTE/plugins/datatables/jquery.dataTables.js"></script>
 <script src="AdminLTE/plugins/datatables-bs4/js/dataTables.bootstrap4.js"></script>
-<!-- AdminLTE App -->
-<script src="AdminLTE/dist/js/adminlte.min.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="AdminLTE/dist/js/demo.js"></script>
 <!-- page script -->
 <script>
     $(function () {
