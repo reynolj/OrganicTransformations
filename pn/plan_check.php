778b9f4235fdd5504f60f92bd19d50882b7d0ad7
reynolj <jakereynolds@csus.edu>
2020-04-17 20:14:57 -0700

    Rewrote ajax request to fix redirection and modal functionality

--- pn/plan_check.php
+++ pn/plan_check.php
@@ -7,9 +7,10 @@
 
     //Authenticate user
     $stmt = $con->prepare(
-      "SELECT p.plan_id, p.plan_name, p1.plan_name AS user_plan_name FROM plans p 
+      "SELECT p.premium_state_id AS premium_id, p.plan_name AS premium_plan_name, p1.plan_name AS user_plan_name, 
+                    u.premium_state AS user_plan_id FROM plans p
                     LEFT JOIN users as u ON (u.user_id = ?)
-                    LEFT JOIN plans as p1 ON (p1.plan_id = u.premium_state)           
+                    LEFT JOIN plans as p1 ON (p1.premium_state_id = u.premium_state)
                     WHERE UPPER(p.plan_name) = ?");
     $stmt->execute([ $_SESSION['user_id'], $tier]);
     $data = $stmt->fetch();
@@ -22,8 +23,8 @@
     // die( "Failed to add break - " . $e->getMessage());
   }
 
-  if ( !$_SESSION['is_admin'] && $data['premium_state'] < $data['plan_id'] ){
-    header("Location: my_plan.php?required_plan=" . $data['plan_name'] . "&user_plan=" . $data['user_plan_name'] );
+  if ( !$_SESSION['is_admin'] && $data['user_plan_id'] < $data['premium_id'] ){
+    header("Location: my_plan.php?required_plan=" . $data['premium_plan_name'] . "&user_plan=" . $data['user_plan_name'] );
     exit();
   }
 ?>
