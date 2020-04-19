bd7cc9d59ce1809924e67ae1c6a454ce3a58f068
jasoncher <jasoncher@gmail.com>
2020-04-18 18:11:43 -0700

    Added "guide_last_modified"

--- pn/api/admin/guides/edit_guide.php
+++ pn/api/admin/guides/edit_guide.php
@@ -12,7 +12,7 @@ try {
     $con = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
     $con->setAttribute(PDO::ATTR_EMULATE_PREPARES, FALSE);
 
-    $stmt = $con->prepare("UPDATE guides SET guide_name = ?, subscription_level = ?, content = ?, thumbnail = ? WHERE guide_id = ?");
+    $stmt = $con->prepare("UPDATE guides SET date_last_modified = now(), guide_name = ?, subscription_level = ?, content = ?, thumbnail = ? WHERE guide_id = ?");
     $stmt->execute([ $_POST['guide_name'], $_POST['subscription_level'], $_POST['content'], $_POST['thumbnail'], $_POST['guide_id'] ]);
     $updated = $stmt->rowCount();
 
